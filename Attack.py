dd6b8ca87b4ceadec37e307e4f292a41a04d3b29
unknown <werty1745@gmail.com>
2023-03-23 13:09:54 +0200

    feat(Attack.py)  - Fixed recovery Time name to accurately represent time units

--- Attack.py
+++ Attack.py
@@ -1,19 +1,19 @@
 from abc import ABC
 class Attack(ABC):
     def __init__(self):
-        self.recoveryTime : int = 0
+        self.recoveryTimeInSeconds : float= 0
         self.capableTargets = []
 
 
 
-    def getRecoveryTime(self):
-        return self.recoveryTime
+    def getRecoveryTimeInSeconds(self):
+        return self.recoveryTimeInSeconds
 
     def getCapableTargets(self):
         return self.capableTargets
 
-    def setRecoveryTime(self,newRecoveryTime):
-        self.recoveryTime=newRecoveryTime
+    def setRecoveryTime(self,newRecoveryTimeInSeconds):
+        self.recoveryTimeInSeconds=newRecoveryTimeInSeconds
 
     def addToCapableTargets(self,newTarget):
         self.capableTargets.append(newTarget)
