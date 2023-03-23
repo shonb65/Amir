33e5a90bdfffd871890bcb9776b14680163aba92
unknown <shonb65@gmail.com>
2023-03-23 12:34:11 +0200

    Added MoonFactory design

--- Moon.py
+++ Moon.py
@@ -1,5 +1,17 @@
 
 
+class MoonFactory:
+    def create_moon(self, moon_type):
+        if moon_type == "full":
+            return FullMoon()
+        elif moon_type == "partial":
+            return PartialMoon()
+        elif moon_type == "red":
+            return RedMoon()
+        else:
+            raise ValueError("Invalid moon type")
+
+
 class Moon():
     pass
 
